56a4266e24c571d0911c5f352a59de6201bae903
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2026-06-30 08:22:36 +0800

    changes to code

--- globals/game_manager.gd
+++ globals/game_manager.gd
@@ -4,5 +4,10 @@ extends Node
 var minigame_ref
 
 func start_minigame():
+	if minigame_ref == null:
+		return
+	
 	var player = get_tree().get_nodes_in_group("Player")[0]
 	player.pop_to_ui(minigame_ref)
+	
+	minigame_ref = null
