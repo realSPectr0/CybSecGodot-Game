16d599ea5419f75d727a2e14c07403c657a291e5
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2026-07-11 22:31:38 +0800

    gaming security init

--- scenes/minigames/gaming_security/gaming_security.gd
+++ scenes/minigames/gaming_security/gaming_security.gd
@@ -1,5 +1,23 @@
 extends Node2D
 
 
+var money = 0
+
+var stage = 1
+var stage_max = 5
+
+
 func _ready() -> void:
 	pass
+
+
+func _physics_process(delta: float) -> void:
+	pass
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	pass
+
+
+func _on_stage_timer_timeout() -> void:
+	$StageTimer.start()
