0d124e908e18902b27cecc0daf02686cb153f122
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2026-07-12 14:20:40 +0800

    sdfdsf

---
+++ scenes/minigames/gaming_security/actors/hacker.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+
+var move_speed = 75.0
+var dir: Vector2
+
+var hp = 4
+
+var can_move = true
+
+
+func _ready() -> void:
+	if dir == Vector2.RIGHT:
+		$AnimatedSprite2D.play('run_right')
+	elif dir == Vector2.LEFT:
+		$AnimatedSprite2D.play('run_left')
+	elif dir == Vector2.UP:
+		$AnimatedSprite2D.play('run_up')
+	elif dir == Vector2.DOWN:
+		$AnimatedSprite2D.play('run_down')
+
+
+func _physics_process(delta: float) -> void:
+	if can_move == false:
+		return
+	
+	global_position += dir * move_speed * delta
+
+
+func _on_area_2d_mouse_entered() -> void:
+	hp -= 1
+	
+	$HitFX.play("flash")
+	
+	if hp <= 0:
+		queue_free()
--- scenes/minigames/gaming_security/gaming_security.gd
+++ scenes/minigames/gaming_security/gaming_security.gd
@@ -8,11 +8,29 @@ var stage_max = 5
 
 
 func _ready() -> void:
-	pass
+	$UI/ProgressBar.max_value = $StageTimer.wait_time
 
 
 func _physics_process(delta: float) -> void:
-	pass
+	$UI/ProgressBar.value = $StageTimer.time_left
+
+
+func spawn_hacker():
+	var random_spawn = $SpawnPoints.get_children().pick_random()
+	
+	var e = load('res://scenes/minigames/gaming_security/actors/hacker.tscn').instantiate()
+	e.global_position = random_spawn.global_position
+	
+	if random_spawn == $SpawnPoints/Left:
+		e.dir = Vector2.RIGHT
+	elif random_spawn == $SpawnPoints/Top:
+		e.dir = Vector2.DOWN
+	elif random_spawn == $SpawnPoints/Bottom:
+		e.dir = Vector2.UP
+	elif random_spawn == $SpawnPoints/Right:
+		e.dir = Vector2.LEFT
+	
+	add_child(e)
 
 
 func _on_gui_input(event: InputEvent) -> void:
@@ -21,3 +39,14 @@ func _on_gui_input(event: InputEvent) -> void:
 
 func _on_stage_timer_timeout() -> void:
 	$StageTimer.start()
+
+
+func _on_hacker_spawn_timer_timeout() -> void:
+	spawn_hacker()
+
+
+func _on_pc_area_area_entered(area: Area2D) -> void:
+	var e = load('res://scenes/minigames/gaming_security/actors/HackingScene.tscn').instantiate()
+	$UI.add_child(e)
+	
+	get_tree().paused = true
