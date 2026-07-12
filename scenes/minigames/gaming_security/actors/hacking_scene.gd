136c91ef0e8ae001ffb358fe21c821e1ce79acd1
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2026-07-12 14:45:30 +0800

    sdfsdf

--- scenes/minigames/gaming_security/actors/hacking_scene.gd
+++ scenes/minigames/gaming_security/actors/hacking_scene.gd
@@ -1,1 +1,15 @@
 extends Control
+
+func _ready() -> void:
+	$Main/ProgressBar.max_value = $Timer.wait_time
+
+
+func _physics_process(delta: float) -> void:
+	$Main/ProgressBar.value = $Timer.wait_time - $Timer.time_left
+	
+	$Code.visible_ratio += delta / 20
+	print($Code.visible_ratio)
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
