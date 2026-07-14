88500affe5c809a1cbd2550a90d18dfd66cfe8ea
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2026-07-14 08:48:22 +0800

    add cats vids

--- scenes/minigames/gaming_security/actors/distraction_window.gd
+++ scenes/minigames/gaming_security/actors/distraction_window.gd
@@ -1,5 +1,15 @@
 extends Panel
 
+var res = [
+	"res://scenes/minigames/gaming_security/art/cat1.tres",
+	"res://scenes/minigames/gaming_security/art/cat2.tres"
+]
+
+
+func _ready() -> void:
+	$AnimatedSprite2D.sprite_frames = load(res.pick_random())
+	$AnimatedSprite2D.play('default')
+
 
 func _on_close_pressed() -> void:
 	queue_free()
