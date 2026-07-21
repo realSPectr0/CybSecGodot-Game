c2f648784dabf8033b659589a76524709f69d8be
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2026-07-21 11:20:22 +0800

    reusing bullets and hurtboxes for persons

--- scenes/minigames/deepfakes/deepfakes.gd
+++ scenes/minigames/deepfakes/deepfakes.gd
@@ -9,12 +9,42 @@ var people_sprite_frames = [
 	load("res://reso/chars_sprite_frames/char_5.tres"),
 ]
 
+var player_bullet_pool = []
+
 
 var spawn_time = 2
 
 
 func _ready() -> void:
 	$SpawnTimer.start(spawn_time)
+	
+	var b = load('res://scenes/minigames/firewalls_and_viruses/actors/objs/PlayerBullet.tscn')
+	for i in 200:
+		var e = b.instantiate()
+		player_bullet_pool.append(e)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and !event.pressed:
+		shoot()
+
+
+func get_active_bullet():
+	for i in player_bullet_pool:
+		if i.is_active == false:
+			return i
+
+
+func shoot():
+	var b = get_active_bullet()
+	
+	if b:
+		b.is_active = true
+		b.dir = $Cannon.global_position.direction_to(get_global_mouse_position())
+		b.global_position = $Cannon.global_position
+		b.look_at(get_global_mouse_position())
+		
+		add_child(b)
 
 
 func spawn_person():
