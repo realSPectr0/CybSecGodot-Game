32315c9990c711128c5db0a3ea866e09c88227d1
rockgem <rockgementiza@gmail.com>
2026-08-12 11:40:36 +0800

    add talk button for quiz persons

--- scenes/quiz_person.gd
+++ scenes/quiz_person.gd
@@ -11,11 +11,7 @@ func _ready() -> void:
 		)
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	GameManager.minigame_title = title
-	GameManager.quiz_path = quiz_path
-	GameManager.quiz_id = id
-	
+func start_dialogue():
 	GameManager.global_player_ref.freeze = true
 	GameManager.global_player_ref.animated_sprite.play('idle_down')
 	
@@ -27,3 +23,25 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	else:
 		var res = load('res://reso/quizzes_learn.dialogue')
 		DialogueManager.show_dialogue_balloon(res, 'start')
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	GameManager.npc_nearby = self
+	GameManager.global_player_ref.talk_button.text = title
+	GameManager.global_player_ref.talk_button.visible = true
+	
+	GameManager.minigame_title = title
+	GameManager.quiz_path = quiz_path
+	GameManager.quiz_id = id
+	
+	#GameManager.global_player_ref.freeze = true
+	#GameManager.global_player_ref.animated_sprite.play('idle_down')
+	#
+	#if GameManager.player_data['passwords'][id] != '':
+		#GameManager.quiz_pass = GameManager.player_data['passwords'][id]
+		#
+		#var res = load('res://reso/quiz_already_done.dialogue')
+		#DialogueManager.show_dialogue_balloon(res, 'start')
+	#else:
+		#var res = load('res://reso/quizzes_learn.dialogue')
+		#DialogueManager.show_dialogue_balloon(res, 'start')
